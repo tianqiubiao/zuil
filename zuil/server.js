6feaa34cbe0d61da0c4155cc99ddbe79ab95b1ed
tianqiubiao <tianqiubiao@xjfbank.com>
2016-05-25 18:15:11 +0800

    20060525

--- zuil/server.js
+++ zuil/server.js
@@ -1,1 +1,11 @@
+charset='utf8';
+var http=require("http");
+var url=require("url");
+var fs=require('fs');
+http.createServer(function(request,response){
+   var urlObj=url.parse(request.url,true);
+   var urlPath=urlObj.pathname;
+   var pathQuery=urlObj.query;
 
+
+}).listen(66666);
